9265ba06ff16cf12598d2a658d339cb09dfa0eda
AhshanHabib26 <habib.net27@gmail.com>
2023-08-23 11:42:37 +0600

    React Router Setup

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,11 @@
+import { RouterProvider } from "react-router-dom";
 import "./App.css";
+import { router } from "./routes/route";
 
 function App() {
   return (
     <>
-      <h1>Hello World</h1>
+      <RouterProvider router={router} />
     </>
   );
 }
